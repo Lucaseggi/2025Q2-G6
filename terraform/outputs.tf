978f5e35242ea40804700a6d1457662fdcfb9366
Gianfranco Magliotti <gmagliotti@itba.edu.ar>
2025-09-08 22:44:36 -0300

    feat: terraform frontend bucket initialize

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -38,3 +38,19 @@ output "queue_private_ip" {
 output "vector_db_private_ip" {
   value = aws_instance.vector-db.private_ip
 }
+
+# Frontend S3 bucket outputs
+output "frontend_bucket_name" {
+  description = "Name of the S3 bucket hosting the frontend"
+  value       = aws_s3_bucket.frontend.bucket
+}
+
+output "frontend_bucket_website_endpoint" {
+  description = "Website endpoint for the frontend S3 bucket"
+  value       = aws_s3_bucket_website_configuration.frontend.website_endpoint
+}
+
+output "frontend_website_url" {
+  description = "Complete URL to access the frontend website"
+  value       = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
+}
